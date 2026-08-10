26fc1156629d1c22daf82fb955fdd392849ef876
İbrahim <ibrahimyazit62@gmail.com>
2026-08-10 11:52:25 +0300

    first commit

---
+++ Task1.py
@@ -0,0 +1,20 @@
+import cv2
+import os
+
+# Açılacak fotoğraf
+image = "image.jpg"
+
+# Fotoğrafı oku
+img = cv2.imread(image)
+
+# Görüntüyü ekranda göster
+cv2.imshow("Kars Kalesi", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Aynı dizine farklı isimle kaydet
+dizin = os.path.dirname(os.path.abspath(image))
+yeni_dosya = os.path.join(dizin, "image_kopya.png")
+
+cv2.imwrite(yeni_dosya, img)
+print(f"Fotoğraf kaydedildi: {yeni_dosya}")
