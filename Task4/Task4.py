13a3a93da6d4132848d8be8c500d2286da9a1cda
İbrahim <ibrahimyazit62@gmail.com>
2026-08-11 17:45:11 +0300

    Update

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -6,15 +6,8 @@ image = "image.jpg"
 # Fotoğrafı oku
 img = cv2.imread(image)
 
-# Dikdörtgenin koordinatları (kendi resmine göre ayarlayabilirsin)
-x1, y1 = 50, 100   # sol üst köşe
-x2, y2 = 100, 50   # sağ alt köşe
-
-# Sarı renk (OpenCV'de BGR formatında: Blue, Green, Red)
-sari_renk = (0, 255, 255)
-
 # İçi dolu dikdörtgen çiz (kalınlık: -1 = dolu)
-cv2.rectangle(img, (x1, y1), (x2, y2), sari_renk, -1)
+cv2.rectangle(img, (50, 100), (100, 50), (0, 255, 255), -1)
 
 # Görüntüyü göster
 cv2.imshow("Sari Cerceveli Alan", img)
