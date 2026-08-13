554611d6d4030efe2bc4dbfd63e7ea7714798964
İbrahim <ibrahimyazit62@gmail.com>
2026-08-13 19:46:50 +0300

    update

--- Task6/Task6.py
+++ Task6/Task6.py
@@ -18,27 +18,9 @@ otsu_val, thresh = cv2.threshold(
 )
 print(f"Otsu'nun bulduğu eşik değeri: {otsu_val}")
 
-# 4. Morphology ile küçük gürültüleri temizle
-kernel = np.ones((5, 5), np.uint8)
-mask_clean = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)   # küçük delikleri kapat
-mask_clean = cv2.morphologyEx(mask_clean, cv2.MORPH_OPEN, kernel)  # küçük gürültü noktalarını sil
+# Sonucu kaydet
+cv2.imwrite('result.jpg', thresh)
 
-# 5. Kontur bul 
-contours, _ = cv2.findContours(
-    mask_clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-)
-largest = max(contours, key=cv2.contourArea) # Burada large contour u bulmamızın sebebi en belirgin tek bir nesneyi ortaya çıkarmak
-print(f"Bulunan kontur sayisi: {len(contours)}")
-print(f"En büyük konturun alani: {cv2.contourArea(largest):.0f} piksel")
-
-# Konturu orijinal görüntü üzerine kırmızı çizgiyle çiz
-result = img.copy()
-cv2.drawContours(result, [largest], -1, (0, 0, 255), 3) # ikinci parametre dizi olmalı o nedenle largest [] içine alındı
-
-# 6. Sonucu kaydet
-cv2.imwrite('result.jpg', result)
-
-cv2.imshow("Result", result)
-cv2.imshow("Normal", img)
+cv2.imshow("Result", thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
