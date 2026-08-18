0aedaaee104b502f6e959c40da78436c2801bee4
İbrahim <ibrahimyazit62@gmail.com>
2026-08-18 09:23:36 +0300

    update

--- Task12/Task12.py
+++ Task12/Task12.py
@@ -8,18 +8,20 @@ cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Kameraya erişilemedi!")
-else:
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Görüntü alınamadı!")
-            break
+    
+while True:
+    ret, frame = cap.read()
+    
+    if not ret:
+        print("Görüntü alınamadı!")
+        break
 
-        cv2.imshow('Kamera Goruntusu', frame)
+    cv2.imshow('Kamera Goruntusu', frame)
 
-        # 'q' tuşuna basılınca çık
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    # 'q' tuşuna basılınca çık
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    cap.release()
-    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
